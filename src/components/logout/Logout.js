51ba9b9b4ee47aff7274e28b051da612daf00e81
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-10-18 19:49:24 -0500

    Se creó el componente Logout

---
+++ src/components/logout/Logout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {useAuth0} from '@auth0/auth0-react';
+
+const Logout = () => {
+    const { logout } = useAuth0();
+    return(
+        <button className="btn btn-danger" onClick={()=>logout({returnTo: window.location.origin})}>Salir</button>
+    )
+};
+
+export default Logout;
