3ee2ad0c0e1e2d5028a501caa6ac263e938767ed
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-10-18 19:47:19 -0500

    Se actualizó la aplicación

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,  Route, Routes} from 'react-router-dom';
+import Navbar from './components/navegacion/Navbar';
+import Inicio from './components/pages/Inicio';
+import Pokemon from './components/pages/Pokemon';
+import Items from './components/pages/Items';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Inicio />}/>
+          <Route path='/carrito' element={<Items />}/>
+        </Routes>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
