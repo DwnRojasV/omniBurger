a2a203cea7fc7b3dcd187d45c06e5735fcfe5fd9
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-10-22 11:08:05 -0500

    se añadieron los productos y se actualizan automaticamente

--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -2,6 +2,7 @@ import React from "react";
 import pagesCss from './pages.css';
 import Carrusel from "../carrusel/Carrusel";
 import Card from "../card/Card";
+import datosProductos from '../../data/productos.json'
 
 const Inicio = () => {
 
@@ -10,36 +11,22 @@ const Inicio = () => {
             <Carrusel />
             <h1 className="pages__titulo">Página de inicio</h1>
             <div className="container productos">
-                <Card
-                    imagen='./logo192.png'
-                    titulo='Nombre del producto'
-                    descripcion='Aquí va la descripción del producto'
-                />
-                <Card
-                    imagen='./logo192.png'
-                    titulo='Nombre del producto'
-                    descripcion='Aquí va la descripción del producto'
-                />
-                <Card
-                    imagen='./logo192.png'
-                    titulo='Nombre del producto'
-                    descripcion='Aquí va la descripción del producto'
-                />
-                <Card
-                    imagen='./logo192.png'
-                    titulo='Nombre del producto'
-                    descripcion='Aquí va la descripción del producto'
-                />
-                <Card
-                    imagen='./logo192.png'
-                    titulo='Nombre del producto'
-                    descripcion='Aquí va la descripción del producto'
-                />
-                <Card
-                    imagen='./logo192.png'
-                    titulo='Nombre del producto'
-                    descripcion='Aquí va la descripción del producto'
-                />
+                {
+                    datosProductos.map(
+                        (producto)=>{
+
+                            return(
+                                <Card
+                                    imagen={producto.urlImagen}
+                                    titulo={producto.nombre}
+                                    descripcion={producto.descripcion}
+                                />
+                            )
+                        }
+                    )
+                }
+                
+
             </div>
         </div>
     )
