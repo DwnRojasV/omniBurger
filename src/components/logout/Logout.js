33e0fe750518e379031a36233116e5feb910d3d9
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-10-23 16:04:27 -0500

    se modificó el logout

--- src/components/logout/Logout.js
+++ src/components/logout/Logout.js
@@ -1,10 +1,11 @@
 import React from "react";
+import logoutCSS from "./logout.css"
 import {useAuth0} from '@auth0/auth0-react';
 
 const Logout = () => {
     const { logout } = useAuth0();
     return(
-        <button className="btn btn-danger" onClick={()=>logout({returnTo: window.location.origin})}>Salir</button>
+        <button className="btn btn-danger logout__boton" onClick={()=>logout({returnTo: window.location.origin})}>Cerrar Sesión</button>
     )
 };
 
